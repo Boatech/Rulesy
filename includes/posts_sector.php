cc6e6a904451b6197bca4df03fa6925dbafdeaa9
Phaxad studios <91748627+phaxadstudios@users.noreply.github.com>
2021-11-30 18:17:32 +0100

    Add files via upload

---
+++ includes/posts_sector.php
@@ -0,0 +1,248 @@
+ <!-- post starts -->
+        <div class="post">
+          <div class="post__top">
+            <a href="javascript:void(0)">
+            <img
+              class="user__avatar post__avatar"
+              src="testimages/profile-3.jpg"
+              alt=""
+            /></a>
+            <div class="post__topInfo">
+              <a href="javascript:void(0)">
+              <h3>Nnamchi Israel</h3>
+            </a>
+              <p>3mins ago</p>
+            </div>
+          </div>
+
+          <div class="post__bottom">
+           <p>Let's work on the post section now and see how it works.I want to see if it's working nice and let's see how we can get our users pictures to fit right in without problems.And let's learn how to make the <kbd>"..."</kbd> or <kbd>see more</kbd>, if the text is too long to fit in ,a maximum of 50 characters before that appears</p>
+          </div>
+
+          <div class="post__image">
+            <a href="javascript:void(0)">
+            <img
+              src="images/dashboard.png" 
+               
+              alt=""
+            />
+          </a>
+          </div>
+
+          <div class="post__options">
+            <div class="post__option">
+              <a href="javascript:void(0)">
+              <img src="images/heart(2).png" width="20px" height="20px">
+              <p>123k</p>
+            </a>
+            </div>
+
+            <div class="post__option">
+              <a href="javascript:void(0)">
+              <img src="images/message.png" width="20px" height="20px">
+              <p>12k</p>
+            </a>
+            </div>
+
+           
+
+            <div class="post__option">
+              <a href="javascript:void(0)">
+               <img src="images/next-page.png" width="20px" height="20px">
+              <p>1k</p>
+              </a>
+            </div>
+            <div class="post__option">
+             <a href="javascript:void(0)">
+              <img src="images/more(2).png" width="20px" height="20px">
+              <p>more</p>
+             </a>
+            </div>
+          </div>
+        </div>
+        <!-- post ends -->
+
+         <!-- post starts -->
+        <div class="post">
+          <div class="post__top">
+            <a href="javascript:void(0)">
+            <img
+              class="user__avatar post__avatar"
+              src="testimages/profile-3.jpg"
+              alt=""
+            />
+          </a>
+            <div class="post__topInfo">
+              <a href="javascript:void(0)">
+              <h3>Nnamchi Israel</h3>
+            </a>
+              <p>4hrs ago</p>
+            </div>
+          </div>
+
+          <div class="post__bottom">
+           <p>Those of you who loves <kbd>gaming</kbd>, get prepared for <code>Rulsey Ultra</code>, we are taking gaming to another whole new <span class="green">Level!</span>. Get ready to experience <code>Rulsey games</code> however you want it!. Play with millions of online users ,you can also buy the new <code>Gamer Lux box</code> to play as well as <code>Gamer Lux VR</code> to join the gaming world of <code>Rulsey</code></p>
+          </div>
+
+          <div class="post__image">
+            <a href="javascript:void(0)">
+            <img
+              src="images/profile-9.jpg" 
+               
+              alt="" height="50%"
+            />
+          </a>
+          </div>
+
+          <div class="post__options">
+            <div class="post__option">
+              <a href="javascript:void(0)">
+              <img src="images/heart(2).png" width="20px" height="20px">
+              <p>343k</p>
+            </a>
+            </div>
+
+            <div class="post__option">
+              <a href="javascript:void(0)">
+              <img src="images/message.png" width="20px" height="20px">
+              <p>232k</p>
+            </a>
+            </div>
+
+           
+
+            <div class="post__option">
+              <a href="javascript:void(0)">
+               <img src="images/next-page.png" width="20px" height="20px">
+              <p>232k</p>
+            </a>
+            </div>
+             <div class="post__option">
+              <a href="javascript:void(0)">
+              <img src="images/more(2).png" width="20px" height="20px">
+              <p>more</p>
+            </a>
+            </div>
+          </div>
+        </div>
+        <!-- post ends -->
+
+        <!-- post starts -->
+       <div class="post">
+          <div class="post__top">
+            <a href="javascript:void(0)">
+            <img
+              class="user__avatar post__avatar"
+              src="testimages/profile-3.jpg"
+              alt=""
+            />
+          </a>
+            <div class="post__topInfo">
+              <a href="javascript:void(0)">
+              <h3>Nnamchi Israel</h3>
+            </a>
+              <p>8hrs ago</p>
+            </div>
+          </div>
+
+          <div class="post__bottom">
+            <p>Awesome <span class="purple">right</span>. Lego superman, i can't wait to create <code>Rulsey for kids</code>, then some adults will be jealous of it, because they can watch cartoons for free but you guys are based on subscription plans to make that happen.</p>
+          </div>
+
+          <div class="post__image">
+            <a href="javascript:void(0)">
+            <img
+              src="images/wp7357832.webp" 
+               
+              alt=""
+            />
+          </a>
+          </div>
+
+          <div class="post__options">
+            <div class="post__option">
+              <a href="javascript:void(0)">
+              <img src="images/heart(2).png" width="20px" height="20px">
+              <p>786k</p>
+            </a>
+            </div>
+
+            <div class="post__option">
+              <a href="javascript:void(0)">
+              <img src="images/message.png" width="20px" height="20px">
+              <p>232k</p>
+            </a>
+            </div>
+
+           
+
+            <div class="post__option">
+              <a href="javascript:void(0)">
+               <img src="images/next-page.png" width="20px" height="20px">
+              <p>245k</p>
+            </a>
+            </div>
+             <div class="post__option">
+              <a href="javascript:void(0)">
+              <img src="images/more(2).png" width="20px" height="20px">
+              <p>more</p>
+            </a>
+            </div>
+          </div>
+        </div>
+        <!-- post ends -->
+
+        <!-- post starts -->
+        <div class="post">
+          <div class="post__top">
+            <a href="javascript:void(0)">
+            <img
+              class="user__avatar post__avatar"
+              src="testimages/profile-3.jpg"
+              alt=""
+            />
+          </a>
+            <div class="post__topInfo">
+              <a href="javascript:void(0)">
+              <h3>Nnamchi Israel</h3>
+            </a>
+              <p>2 days ago</p>
+            </div>
+          </div>
+
+          <div class="post__bottom">
+            <p>Well looks like we will be on the front-end till <span class="purple">December</span>, while the new year starts with new thoughts and advancement. I should be finished on this before April and i hope to launch it on my <code>Birthday</code>, so when <span class="amber">Rulsey</span> celebrates and anniversary, i celebrate my  birthday</p>
+          </div>
+
+          <div class="post__options">
+            <div class="post__option">
+              <a href="javascript:void(0)">
+              <img src="images/heart(2).png" width="20px" height="20px">
+              <p>123m</p>
+            </a>
+            </div>
+
+            <div class="post__option">
+              <a href="javascript:void(0)">
+              <img src="images/message.png" width="20px" height="20px">
+              <p>554k</p>
+            </a>
+            </div>
+
+           
+
+            <div class="post__option">
+              <a href="javascript:void(0)">
+               <img src="images/next-page.png" width="20px" height="20px">
+              <p>132k</p>
+            </a>
+            </div>
+             <div class="post__option">
+              <a href="javascript:void(0)">
+              <img src="images/more(2).png" width="20px" height="20px">
+              <p>more</p>
+              </a>
+            </div>
+          </div>
+        </div>
+        <!-- post ends -->
---
+++ includes/stories_section.php
@@ -0,0 +1,115 @@
+<div class="storyReel">
+  <style type="text/css">
+    .storyReel{
+      color: #fff;
+      background: #171717;
+    }
+
+  </style>
+           <style type="text/css">
+              .lover-story:hover{
+                font-size: 40px;
+              }
+            </style>
+           <!-- story starts -->
+         
+        
+           <!-- story starts -->
+          <div
+            style="
+              background-image: url('testimages/story-3.jpg');
+            "
+            class="story"
+          >
+            <img
+              class="user__avatar story__avatar"
+              src="testimages/profile-3.jpg"
+              alt=""
+            />
+            <p><img src="images/heart(2).png" width="30px" height="30px" style="bottom: 2px;"></p>
+          </div>
+          <!-- story ends -->
+
+          <!-- story starts -->
+          <div
+            style="
+              background-image: url('testimages/story-1.jpg');
+            "
+            class="story"
+          >
+            <img
+              class="user__avatar story__avatar"
+              src="testimages/profile-2.jpg"
+              alt=""
+            />
+            <p class="lover-story"><img src="images/heart(2).png" width="30px" height="30px" style="bottom: 2px;"></p>
+            
+          </div>
+          <!-- story ends -->
+
+          <!-- story starts -->
+          <div
+            style="
+              background-image: url('testimages/story-2.jpg');
+            "
+            class="story"
+          >
+            <img
+              class="user__avatar story__avatar"
+              src="testimages/profile-3.jpg"
+              alt=""
+            />
+            <p class="lover-story"><img src="images/heart(2).png" width="30px" height="30px" style="bottom: 2px;"></p>
+            
+          </div>
+          <!-- story ends -->
+
+          <!-- story starts -->
+          <div
+            style="
+              background-image: url('testimages/story-4.jpg');
+            "
+            class="story"
+          >
+            <img
+              class="user__avatar story__avatar"
+              src="testimages/profile-4.jpg"
+              alt=""
+            />
+           <p class="lover-story"><img src="images/heart(2).png" width="30px" height="30px" style="bottom: 2px;"></p>
+          </div>
+          <!-- story ends -->
+
+          <!-- story starts -->
+          <div
+            style="
+              background-image: url('testimages/story-5.jpg');
+            "
+            class="story"
+          >
+            <img
+              class="user__avatar story__avatar"
+              src="testimages/profile-5.jpg"
+              alt=""
+            />
+            <p class="lover-story"><img src="images/heart(2).png" width="30px" height="30px" style="bottom: 2px;"></p>
+            
+          </div>
+          <!-- story ends -->
+
+          <!-- story starts -->
+          <div
+            style="
+              background-image: url('testimages/story-6.jpg');
+            "
+            class="story"
+          >
+            <img src="testimages/profile-20.jpg" 
+              class="user__avatar"
+              alt=""
+            />
+
+            <p class="lover-story"><a class="lover-story"><img src="images/heart(2).png" width="30px" height="30px" style="bottom: 2px;"></a></p>
+          </div>
+          <!-- story ends -->
+        </div>
