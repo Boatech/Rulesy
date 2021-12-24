56e4b7b1a21d49198eccfb5d368bf375c8d7d0f0
Apaxaa Studios <91748627+phaxadstudios@users.noreply.github.com>
2021-12-24 07:07:07 +0100

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,2 +1,5 @@
 <?php 
-$con = mysqli_connect("localhost", "root", "bloomonlineshop");
+$host_name = "localhost";
+$username_acess_name = "root";
+$password = "";
+$con = mysqli_connect($host_name, $username, $password);
