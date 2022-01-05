a0c1d84ea1d54625b9fb627ce091944c7f8d9d19
Boatech <70201231+Boatech@users.noreply.github.com>
2022-01-05 22:35:38 +0100

    Delete includes directory

--- includes/sidebars_specials.php
+++
@@ -1,8 +0,0 @@
- <div class="sidebarRow ">
-        	<img src="images/bar-chart.png" width="40px" height="40px" />
-        	<p> &nbsp; Platforms <img src="images/new.png" width="30px" height="30px"></p></p>
-
-        </div>
-        <br>
-
-        <!-- for footer  -->
