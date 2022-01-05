bfa06dadbe7930a0b2aa690a5d3b57d1c4d936d1
Nnamchi Israel <91748627+phaxadstudios@users.noreply.github.com>
2022-01-06 00:39:03 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,14 @@
 <?php
 //redirects user to the main scripts ...
-if (!isset($_SERVER['HTTPS'])) {
 
+//checking if the connection is from a 'https' secured connection...
+if (!isset($_SERVER['HTTPS'])) {
+//storing the local page url
 $url= 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+//redirecting users to the public folder
 header("Location: public/", TRUE, 301);
 exit();
-die();
+die("Please try refreshing this page...");
 }
 ?>
 
