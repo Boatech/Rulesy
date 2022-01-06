a5a03407bbd714121071bea171cccc990020663b
Nnamchi Israel <91748627+phaxadstudios@users.noreply.github.com>
2022-01-06 01:20:16 +0100

    Create head-tags.php

---
+++ Public/paritals/head-tags.php
@@ -0,0 +1,61 @@
+    <!-- here lies the head tags for Rulsey development -->
+    <title>Rulsey</title>
+    <meta charset="UTF-8" />
+    <meta http-equiv="x-ua-compatible" content="ie=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="robots" content="index, follow" />
+    <meta name="description" content="Rulsey is a fun to use social media platform">
+    <meta property="og:title" content="Rulsey">
+    <meta property="og:url" content="https://www.rulsey.com">
+    <meta property="og:local" content="en_US">
+    <meta property="og:site_name" content="rulsey">
+    <meta property="og:type" content="website">
+    <meta property="og:description" content="Rulsey is a fun to use social media platform">
+    <meta property="og:image" content="./assets/img/favicon.ico">
+    <meta name="theme-color" content="#ffffff">
+    <meta name="keywords" content="">
+    <meta name="referrer" content="strict-origin-when-cross-origin">
+    <meta http-equiv="Content-Language" content="en_US" />
+    <meta name="hostname" content="phaxad.com">
+    <meta property="og:image" content="https://phaxad.com/imageD/logo.png" />
+    <meta property="og:image:width" content="1280" />
+    <meta property="og:image:height" content="640" />
+    <meta property="og:image:type" content="image/png" />
+    <meta property="og:url" content="https://phaxad.com" />
+    <meta property="og:title" content="The home of development" />
+    <meta property="og:description" content="" />
+    
+    <link rel="stylesheet" href="./assets/css/tailwind.output.css" />
+
+   
+    <link rel="stylesheet" href="https://unpkg.com/@themesberg/flowbite@1.2.0/dist/flowbite.min.css" />
+  
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.css"
+    />
+     <link rel="icon" type="image/x-icon" href="./assets/img/favicon.ico">
+
+   
+<link
+    href="https://fonts.googleapis.com/icon?family=Material+Icons"
+    rel="stylesheet"
+/>
+
+<link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"
+    integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w=="
+    crossOrigin="anonymous"
+/>
+
+
+  
+<link
+      href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap"
+      rel="stylesheet"
+    />
+   
+    <url path="data:category{'data'}" route="mainstream.ols" include="session_start(),ob_start(),session_set_cookie_params['time_duration']"></url>
+    <url path="link['os::path']" margination="data()" lang="var['language']"></url>
+    <url path="validatate.oponent['inject::unconstruct']" route="banana.ols" include="none"></url>
