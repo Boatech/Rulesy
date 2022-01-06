bfb083e215e3a1c2dc36f54a4cafb34054749402
Nnamchi Israel <91748627+phaxadstudios@users.noreply.github.com>
2022-01-06 01:25:55 +0100

    Create footer.php

---
+++ Public/paritals/footer.php
@@ -0,0 +1,15 @@
+<!-- the  footer contains javascript scripts, embedded for speed load -->
+ <script
+      src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js"
+      defer
+    ></script>
+     <script src="https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.js"></script>
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script src="https://unpkg.com/@themesberg/flowbite@1.2.0/dist/flowbite.bundle.js"></script>
+    <script src="./assets/js/init-alpine.js"></script>
+     <script
+      src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js"
+      defer
+    ></script>
+<!--end of footer scripts -->
