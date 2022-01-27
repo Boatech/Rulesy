584fe52379e118259ced21ff17d687254ab37d19
Nnamchi Israel <91748627+phaxadstudios@users.noreply.github.com>
2022-01-27 20:49:05 +0100

    Delete nocache.construct.php

--- Public/paritals/nocache.construct.php
+++
@@ -1,7 +0,0 @@
-<?php
-    // Date in the past change later when using a dynamic caching system
-    //this prevents caching, use dynamic caching system like varnish or memecache
-    header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
-    header("Cache-Control: no-cache");
-    header("Pragma: no-cache");
-?>
