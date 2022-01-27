0a6ab7933b9d53aff4839ef5a2a2bc62f6cf7273
Nnamchi Israel <91748627+phaxadstudios@users.noreply.github.com>
2022-01-27 20:49:45 +0100

    Create meta-for-index.php

---
+++ Public/paritals/includes_meta/meta-for-index.php
@@ -0,0 +1,9 @@
+<title><?php echo $user_data->username;?></title>
+<meta data-react-helmet="true" property="og:title" content="<?php echo $user_data->username;?>"/>
+<meta data-react-helmet="true" property="og:url" content="https://www.rulsey.com"/>
+<meta  data-react-helmet="true" name="author" content="<?php echo $user_data->username;?>"/>
+<meta data-react-helmet="true" property="og:description" content="Welcome to rulsey, watch videos, make posts and enjoy chatting with your friends with new technology. "/>
+<meta data-react-helmet="true" name="hostname" content="rulsey.com" />
+<meta data-react-helmet="true" name="user-login" content="<?php echo $user_data->username;?>" />
+<meta data-react-helmet="true" name="page-subject" content="rulsey.com" />
+<?php include 'http://127.0.0.1/phaxad/rulsey/public/paritals/home_includes/meta_data.php'; ?>
