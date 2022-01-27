83c5e24c1bce7429f5c73dec8cd15cfc72e5a658
Nnamchi Israel <91748627+phaxadstudios@users.noreply.github.com>
2022-01-27 20:53:58 +0100

    Create path.php

---
+++ Public/path.php
@@ -0,0 +1,12 @@
+<?php
+/* This part folder belongs to Olsen team Web developers
+* It purpose to to allow definite path ,to allow things follow easily.
+* coypright Apaxaa Studios 
+* 2022 - 2024 
+* contact Olsen team for more..
+*/
+define("ROOT_PATH", realpath(dirname(__FILE__)));
+
+//change the part to what ever path it is set in your system...
+//eg define("BASE_URL", "http://localhost/foldername");
+define("BASE_URL", "http://127.0.0.1/phaxad/rulsey/public");
