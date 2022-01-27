84e1b3e8755dd119e00ca7868eab7c20f663f828
Nnamchi Israel <91748627+phaxadstudios@users.noreply.github.com>
2022-01-28 00:04:06 +0100

    Create notes.construct.php

---
+++ Public/assets/php/notes.construct.php
@@ -0,0 +1,12 @@
+<?php 
+
+/**
+ * how to resize images without losing it's quality
+ * how to use asynchronize loading to load posts, to avoid very slow loading
+ * load all images from https://www.rulseyprojects.com/docs/images/ 
+ * load all user's images from https://www.rulseyprojects.com/docs/users/images/
+ * rulseyproject should be open source, and runs on CDN itself
+ * rulseyproject is an open source platform that powers Rulsey designs, front-end as well as minor backend developers, it is a platform
+ * where developers can make suggestions and build features they want to be added to the social media
+ * contributors which are mostly developers would be signed up on https://rulsey.phaxad.com/developers/
+ * contributions are made on Phaxad as well as future work..**/
