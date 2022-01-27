b128aa2093edd47442d278344491720f3067e0e8
Nnamchi Israel <91748627+phaxadstudios@users.noreply.github.com>
2022-01-27 20:45:48 +0100

    Create stories_section.php

---
+++ Public/paritals/home_includes/stories_section.php
@@ -0,0 +1,101 @@
+<div class="storyReel dark:bg-gray-700 ">
+
+           <!-- story starts -->
+          <div
+            style="
+              background-image: url('./assets/img/testimages/story-3.jpg');
+            "
+            class="story"
+          >
+            <img
+              class="user__avatar story__avatar"
+              src="http://127.0.0.1/phaxad/friend/profile_images/<?php echo $user_data->user_image; ?>"
+              alt="<?php echo $user_data->username;?>"
+            />
+            <p><img src="./assets/img/images/heart(2).png" class="border-0" width="30px" height="30px" style="bottom: 2px;"></p>
+          </div>
+          <!-- story ends -->
+
+          <!-- story starts -->
+          <div
+            style="
+              background-image: url('./assets/img/testimages/story-1.jpg');
+            "
+            class="story"
+          >
+            <img
+              class="user__avatar story__avatar"
+              src="./assets/img/testimages/profile-2.jpg"
+              alt=""
+            />
+            <p class="lover-story"><img src="./assets/img/images/heart(2).png" class="border-0" width="30px" height="30px" style="bottom: 2px;"></p>
+            
+          </div>
+          <!-- story ends -->
+
+          <!-- story starts -->
+          <div
+            style="
+              background-image: url('./assets/img/testimages/story-2.jpg');
+            "
+            class="story"
+          >
+            <img
+              class="user__avatar story__avatar"
+              src="./assets/img/testimages/profile-3.jpg"
+              alt=""
+            />
+            <p class="lover-story"><img src="./assets/img/images/heart(2).png" class="border-0" width="30px" height="30px" style="bottom: 2px;"></p>
+            
+          </div>
+          <!-- story ends -->
+
+          <!-- story starts -->
+          <div
+            style="
+              background-image: url('./assets/img/testimages/story-4.jpg');
+            "
+            class="story"
+          >
+            <img
+              class="user__avatar story__avatar"
+              src="./assets/img/testimages/profile-4.jpg"
+              alt=""
+            />
+           <p class="lover-story"><img src="./assets/img/images/heart(2).png" class="border-0" width="30px" height="30px" style="bottom: 2px;"></p>
+          </div>
+          <!-- story ends -->
+
+          <!-- story starts -->
+          <div
+            style="
+              background-image: url('./assets/img/testimages/story-5.jpg');
+            "
+            class="story"
+          >
+            <img
+              class="user__avatar story__avatar"
+              src="./assets/img/testimages/profile-5.jpg"
+              alt=""
+            />
+            <p class="lover-story"><img src="./assets/img/images/heart(2).png" class="border-0"  width="30px" height="30px" style="bottom: 2px;"></p>
+            
+          </div>
+          <!-- story ends -->
+
+          <!-- story starts -->
+          <div
+            style="
+              background-image: url('./assets/img/testimages/story-6.jpg');
+            "
+            class="story"
+          >
+            <img src="./assets/img/testimages/profile-20.jpg" 
+              class="user__avatar"
+              alt=""
+            />
+
+            <p class="lover-story"><a class="lover-story"><img src="./assets/img/images/heart(2).png" class="border-0" width="30px" height="30px" style="bottom: 2px;"></a></p>
+          </div>
+          <!-- story ends -->
+        </div>
