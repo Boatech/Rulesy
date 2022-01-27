0a93fc48e76cdc177ec5f1d9d0d22f33044f3e3c
Nnamchi Israel <91748627+phaxadstudios@users.noreply.github.com>
2022-01-27 20:44:16 +0100

    Create sidebar_rows.php

---
+++ Public/paritals/home_includes/sidebar_rows.php
@@ -0,0 +1,97 @@
+<div class="sidebarRow">
+          <a href="javascript:void(0)"><img src="./assets/img/images/question(1).png" width="40px" height="40px" class="border-0"></a>
+          <a href="javascript:void(0)">
+          <p> &nbsp;Help center</p>
+        </a>
+        </div>
+
+        <div class="sidebarRow">
+          <a href="javascript:void(0)">
+          <img src="./assets/img/images/page-views.png" class="border-0" width="40px" height="40px">
+        </a>
+        <a href="javascript:void(0)">
+          <p>  &nbsp;Pages</p>
+        </a>
+        </div>
+
+      
+
+        <div class="sidebarRow">
+          <a href="javascript:void(0)">
+          <img src="./assets/img/images/message(1).png" width="40px" height="40px" class="border-0">
+        </a>
+        <a href="javascript:void(0)">
+          <p> &nbsp;Chat</p>
+        </a>
+        </div>
+
+        <div class="sidebarRow">
+          <a href="javascript:void(0)">
+        	<img src="./assets/img/images/notification(2).png" width="40px" height="40px" class="border-0"></a>
+          <a href="javascript:void(0)">
+        		<p> Notifications</p>
+        	</a>
+        </div>
+
+        <div class="sidebarRow">
+          <a href="javascript:void(0)">
+          <img src="./assets/img/images/shopping-bag.png" width="40px" height="40px" class="border-0"></a>
+          <a href="javascript:void(0)">
+          <p> &nbsp;Shop</p>
+        </a>
+        </div>
+
+
+       
+
+        <div class="sidebarRow">
+        	<img src="./assets/img/images/bionic-eye.png" width="40px" height="40px" class="border-0">
+
+          <style type="text/css">
+            .mode-a{
+              text-decoration: none;
+              color: inherit;
+              background: inherit;
+            }
+          </style>
+        	<p><a id="theme-toggle" style="background: inherit; color: inherit;"  href="javascript:void(0)">&nbsp;Mode</a></p>
+        </div>
+
+         <div class="sidebarRow">
+           <a href="javascript:void(0)">
+          <img src="./assets/img/images/settings.png" width="40px" height="40px"></a>
+          <p>&nbsp; <a href="javascript:void(0)">Settings</a></p>
+        </div>
+
+        <div class="sidebarRow">
+           <a href="javascript:void(0)">
+          <img src="./assets/img/images/letter-a.png" width="30px" height="30px"></a>
+          <p> &nbsp; <a href="javascript:void(0)">More</a></p>
+        </div>
+
+        <style type="text/css">
+        	.colormix{
+        		color: purple;
+        	}
+        </style>
+
+         <div class="sidebarRow ">
+           <a href="javascript:void(0)">
+          <img src="./assets/img/images/video-editing-app.png" width="40px" height="40px"></a>
+          <p> &nbsp; <a href="javascript:void(0)">StreamLine </a> <a href="javascript:void(0)"><img src="./assets/img/images/new.png" width="30px" height="30px"></a></p>
+        </div>
+
+           <div class="sidebarRow ">
+          <img src="./assets/img/images/bar-chart(1).png" width="40px" height="40px">
+          <p> &nbsp;Business <img src="./assets/img/images/new.png" width="30px" height="30px"></p>
+        </div>
+
+        <div class="sidebarRow ">
+        	<img src="./assets/img/images/quality.png" width="40px" height="40px">
+        	<p>Trax <img src="./assets/img/images/new.png" width="30px" height="30px"></p>
+        </div>
+
+        <div class="sidebarRow ">
+          <img src="./assets/img/images/world-globe.png" width="40px" height="40px">
+          <p> &nbsp;Language settings <img src="./assets/img/images/new.png" width="30px" height="30px"></p>
+        </div><br>
