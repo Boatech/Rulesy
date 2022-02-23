24793a15e11f40338283853ae15fa3dfbe06b820
Nnamchi Israel <91748627+phaxadstudios@users.noreply.github.com>
2022-02-23 03:22:01 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
 //redirects user to the main scripts ...
+//This script would be removed before launch and the public folder would be the website public folder
 
-//checking if the connection is from a 'https' secured connection...
+/**checking if the connection is from a 'https' secured connection, however it does not work, it only redirects... 
+this is due to the fact it is hosted on localhost at the moment **/
 if (!isset($_SERVER['HTTPS'])) {
 //storing the local page url
 $url= 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
